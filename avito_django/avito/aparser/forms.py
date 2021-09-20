08392effb5930a41fa8d41abe1a3e273eccc8a68
KAA-sign <q89308033587@gmail.com>
2021-09-20 16:37:32 +0300

    +

--- avito_django/avito/aparser/forms.py
+++ avito_django/avito/aparser/forms.py
@@ -8,5 +8,5 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ('title', 'price', 'url')
-        widgets = {'title': forms.TextInput,}
+        fields = ('title', 'price', 'currency', 'url')
+        widgets = {'title': forms.TextInput, 'currency': forms.TextInput}
